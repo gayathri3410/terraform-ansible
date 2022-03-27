f9918752b277ff904533e706c48a6eb18c8837ca
gayathri <gayathri3410@gmail.com>
2022-03-27 11:16:26 +0530

    adding

--- versions.tf
+++ versions.tf
@@ -6,19 +6,19 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 2.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.0"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 3.0"
-    }
+    # random = {
+    #   source  = "hashicorp/random"
+    #   version = ">= 3.0"
+    # }
+    # null = {
+    #   source  = "hashicorp/null"
+    #   version = ">= 3.0"
+    # }
   }
   # Terraform State Storage to Azure Storage Container (Values will be taken from Azure DevOps)
-  backend "azurerm" {
+  # backend "azurerm" {
 
-  }
+  # }
 }
 
 # Provider Block
